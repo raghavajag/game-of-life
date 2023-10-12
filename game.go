10651d4eb76f6401cf4eb9eb00b24185347f0eaa
raghavajag <raghavajagarwal@gmail.com>
2023-10-13 04:55:33 +0530

    fix: preventing multiple clicks on the same cell

--- game.go
+++ game.go
@@ -16,14 +16,38 @@ type Event struct {
 }
 
 type Game struct {
-	Screen tcell.Screen
-	Board  *Board
-	stop   bool
-	hide   bool
-	Ticker *time.Ticker
-	Event  chan Event
-	paused bool
-	draw   bool
+	Screen      tcell.Screen
+	Board       *Board
+	stop        bool
+	hide        bool
+	Ticker      *time.Ticker
+	Event       chan Event
+	paused      bool
+	draw        bool
+	clickedCell *Cell
+}
+
+func (g *Game) drawGrid() {
+	g.Screen.Clear()
+
+	for y, row := range g.Board.grid {
+		for x, alive := range row {
+			// Adjust cell coordinates to screen coordinates
+			screenX := x * 2
+			screenY := y
+
+			if alive.state {
+				st := tcell.StyleDefault.Background(tcell.ColorWhite)
+				g.Screen.SetContent(screenX, screenY, ' ', nil, st)
+				g.Screen.SetContent(screenX+1, screenY, ' ', nil, st)
+			} else {
+				st := tcell.StyleDefault.Background(tcell.ColorBlack)
+				g.Screen.SetContent(screenX, screenY, ' ', nil, st)
+				g.Screen.SetContent(screenX+1, screenY, ' ', nil, st)
+			}
+		}
+	}
+	g.Screen.Show()
 }
 
 func (g *Game) display() {
@@ -45,16 +69,34 @@ func (g *Game) display() {
 func (g *Game) Loop() error {
 	for {
 		g.display()
+		// g.drawGrid()
 		select {
 		case ev := <-g.Event:
 			switch ev.Type {
 			case "switchState":
 				if (ev.X < g.Board.width) && (ev.Y < g.Board.height) && (ev.X >= 0) && (ev.Y >= 0) {
-					{
-						g.Board.Get(ev.X, ev.Y).Switch()
+					cell := g.Board.Get(ev.X, ev.Y)
+
+					// If a cell is already clicked, ignore further clicks until the mouse button is released.
+					if g.clickedCell != nil {
+						if g.clickedCell == cell {
+							// Same cell clicked again, do nothing.
+							continue
+						}
 					}
+
+					// Otherwise, track the new cell and toggle its state.
+					g.clickedCell = cell
+					cell.Switch()
 				}
 				g.Screen.Show()
+			// case "switchState":
+			// 	if (ev.X < g.Board.width) && (ev.Y < g.Board.height) && (ev.X >= 0) && (ev.Y >= 0) {
+			// 		{
+			// 			g.Board.Get(ev.X, ev.Y).Switch()
+			// 		}
+			// 	}
+			// 	g.Screen.Show()
 			case "step":
 				if !g.paused {
 					g.Board.Next()
